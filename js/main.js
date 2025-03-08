fe48d3e00d3a31576db793da1063acbd8d9a2618
prototype888 <146809518+prototype888@users.noreply.github.com>
2025-03-08 21:54:46 +0700

    Update main.jsuhuk

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('FOR KAK VELL💐💐').split('')
+    const titles = (' FOR KAK VELL 💐💐 ').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
